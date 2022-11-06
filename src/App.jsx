5551f3066db0ea08245ff5fb070a46237093368e
NBDuy <nbduy2001@gmail.com>
2022-11-06 19:21:31 +0700

    Add axios + button to load memes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import axios from "axios";
 
 function App() {
-  return <h1>Test</h1>;
+  const [memes, setMemes] = React.useState([]);
+
+  const getData = async () => {
+    try {
+      const resp = await axios.get("https://api.imgflip.com/get_memes");
+      setMemes(resp.data.data.memes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    handleLoadMeme();
+  }, []);
+
+  const handleLoadMeme = () => {
+    getData();
+  };
+
+  return (
+    <div className="page-container">
+      <button onClick={handleLoadMeme}>Show memes</button>
+      {memes.map((item) => (
+        <div key={item.id} className="meme-container">
+          <img className="meme" src={item.url} alt="" />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
