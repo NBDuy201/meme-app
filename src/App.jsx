6c3d10ad2f4589fec6b736c4644d199415f8dd11
NBDuy <nbduy2001@gmail.com>
2022-11-09 09:39:05 +0700

    Deploy on Github Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import MemeList from "./components/MemeList";
 
 const queryClient = new QueryClient();
@@ -10,7 +9,6 @@ function App() {
     <div className="page-container">
       <QueryClientProvider client={queryClient}>
         <MemeList></MemeList>
-        <ReactQueryDevtools></ReactQueryDevtools>
       </QueryClientProvider>
     </div>
   );
