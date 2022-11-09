f8bda92701b62360dde7533cb94ea3c9001722eb
NBDuy <nbduy2001@gmail.com>
2022-11-09 09:09:53 +0700

    Update fetch data react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import MemeList from "./components/MemeList";
 
 const queryClient = new QueryClient();
@@ -9,6 +10,7 @@ function App() {
     <div className="page-container">
       <QueryClientProvider client={queryClient}>
         <MemeList></MemeList>
+        <ReactQueryDevtools></ReactQueryDevtools>
       </QueryClientProvider>
     </div>
   );
--- src/components/MemeList.jsx
+++ src/components/MemeList.jsx
@@ -8,13 +8,14 @@ import { Button, createTheme, ImageList, ImageListItem } from "@mui/material";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#D6D2DA",
+      main: "#D2D6DA",
     },
   },
 });
 
 const getData = async () => {
   try {
+    console.log("fetch");
     const resp = await axios.get("https://api.imgflip.com/get_memes");
     return resp.data.data.memes;
   } catch (error) {
@@ -23,20 +24,7 @@ const getData = async () => {
 };
 
 const MemeList = () => {
-  const { data, status, isLoading, error } = useQuery("memes", getData);
-
-  const [memes, setMemes] = React.useState([]);
-
-  React.useEffect(() => {
-    handleLoadMeme();
-  }, [status, data]);
-
-  const handleLoadMeme = () => {
-    console.log("run");
-    if (status === "success") {
-      setMemes(data);
-    }
-  };
+  const { data, isLoading, error, refetch } = useQuery("memes", getData);
 
   // Hanle loading
   if (isLoading) return "Loading...";
@@ -50,16 +38,17 @@ const MemeList = () => {
         theme={theme}
         variant="contained"
         sx={{ fontWeight: "bold" }}
-        onClick={() => handleLoadMeme()}
+        onClick={refetch}
       >
         Show memes
       </Button>
       <ImageList variant="masonry" cols={4} gap={20}>
-        {memes.map((item) => (
-          <ImageListItem key={item.id}>
-            <Meme data={item}></Meme>
-          </ImageListItem>
-        ))}
+        {data &&
+          data.map((item) => (
+            <ImageListItem key={item.id}>
+              <Meme data={item}></Meme>
+            </ImageListItem>
+          ))}
       </ImageList>
     </>
   );
